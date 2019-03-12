0ebc5a3a7965819bc434f8d5dd90dcfb394aab30
Lorenzo Llamas <ltl924@gmail.com>
2019-03-12 22:12:06 +0000

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,29 @@
 def division(num1, num2)
- 
+  return num1 / num2 
 end
+#division(12,2)
 
 def assign_variable(value)
-  
+  my_name = value
 end
 
-def argue
-  
+def argue(evidence)
+  return evidence
 end
 
-def greeting
+def greeting(greeting,name)
 
 end
 
 def return_a_value
-
+  return "Nice"
 end
 
 def last_evaluated_value
-
+  return "expert"
 end
 
-def pizza_party
-
+def pizza_party(topping="cheese")
+  puts topping
+  return topping
 end
